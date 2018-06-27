20a61900a5d25630ea10d8b69b632db527c05dd0
UxGood <uxgood.org@gmail.com>
2018-06-27 15:00:44 +0800

    use xml instead yaml for config file

--- DependencyInjection/UxGoodOAuthExtension.php
+++ DependencyInjection/UxGoodOAuthExtension.php
@@ -22,8 +22,8 @@ class UxGoodOAuthExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yaml');
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('oauth.xml');
     }
 
     /**
